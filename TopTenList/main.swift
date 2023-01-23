ce37795fa5a79caf2ff25577a7fadb3042ac786f
Morgan <Mharrisstoertz@gmail.com>
2023-01-23 09:28:05 -0500

    most efficient iteration

--- TopTenList/main.swift
+++ TopTenList/main.swift
@@ -9,13 +9,15 @@ import Foundation
 
 let favouriteAnimals = ["Alpaca", "Zebra", "Flamingo", "Tiger", "Giraffe", "House Wren", "Racoon", "Skunk", "Chicken", "White-tailed Deer" ]
 
-print(favouriteAnimals[0])
-print(favouriteAnimals[4])
-print(favouriteAnimals[9])
 
 //iteration
 
 for i in 0...9 {
     print(favouriteAnimals [i])
 }
+
+for animal in favouriteAnimals{
+    print(animal)
+}
+        
         
